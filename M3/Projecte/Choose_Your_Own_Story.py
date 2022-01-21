18c0bc86125bcbae69ccc89eb81727ac3909016a
Brahian412 <95907379+Brahian412@users.noreply.github.com>
2022-01-21 13:48:06 +0100

    Update Choose_Your_Own_Story.py

--- M3/Projecte/Choose_Your_Own_Story.py
+++ M3/Projecte/Choose_Your_Own_Story.py
@@ -178,7 +178,7 @@ while True:
         list_users_ids = funciones.getUserIds()
         id_user = list_users_ids[1][list_users_ids[0].index(username)]
         funciones.insertCurrentGame(id_user, char, adv)
-        funciones.insertCurrentChoice(game_context["id_game"], 1, 1)
+        funciones.insertCurrentChoice(game_context["id_game"], 1, 1,game_context["id_adventure"])
         fstep = funciones.get_first_step_adventure()
         # Tupla que contiene los id_options siguientes
         tup_options = fstep["answer_in_step"]
@@ -189,20 +189,25 @@ while True:
         print(fstep["Description"])
         print("Options:\n")
 
-        for i in tup_nextstep:
-            stringOptions += "{})".format(i) + idAnswers_ByStep_Adventure[(i, 1)]["Description"] + "\n"
+        # For para crear una lista con las opciones, para que no salga al paso al que vas
+        list_options = []
+        for i in range(1, len(tup_nextstep)+1):
+            list_options.append(i)
 
-        opc = funciones.getOpt(textOpts=stringOptions, inputOptText="Select Option ->", rangeList=list(tup_nextstep), )
+        for i in tup_nextstep:
+            stringOptions += "{})".format(tup_nextstep.index(i)+1) + idAnswers_ByStep_Adventure[(i, 1)]["Description"] + "\n"
+
+        opc = funciones.getOpt(textOpts=stringOptions, inputOptText="Select Option ->", rangeList=list_options, )
         funciones.insertCurrentChoice(game_context["id_game"], game_context["id_step"],
-                                      tup_options[tup_nextstep.index(opc)])
-        print(idAnswers_ByStep_Adventure[(opc, game_context["id_step"])]["Resolution_Answer"])
+                                      tup_options[tup_nextstep.index(tup_nextstep[opc-1])],game_context["id_adventure"])
+        print(idAnswers_ByStep_Adventure[(tup_nextstep[opc-1], game_context["id_step"])]["Resolution_Answer"])
         print()
         input("Press Enter to continue")
-        game_context["id_step"] = opc
+        game_context["id_step"] = tup_nextstep[opc-1]
         funciones.insertCurrentChoice(game_context["id_game"], game_context["id_step"],
-                                      tup_options[tup_nextstep.index(opc)])
-        idAnswers_ByStep_Adventure = funciones.get_answers_bystep_adventure()
+                                      tup_options[tup_nextstep.index(tup_nextstep[opc-1])],game_context["id_adventure"])
 
+        idAnswers_ByStep_Adventure = funciones.get_answers_bystep_adventure()
         while True:
             if id_by_steps[game_context['id_step']]["Final_Step"] == 0:
                 tup_options = id_by_steps[game_context["id_step"]]["answer_in_step"]
@@ -211,25 +216,32 @@ while True:
                 funciones.getHeader(adventures[adv]["Name"])
                 print(id_by_steps[game_context["id_step"]]["Description"])
                 print("Options:")
+
+                #For para crear una lista con las opciones, para que no salga al paso al que vas
+                list_options = []
+                for i in range(1,len(tup_nextstep)+1):
+                    list_options.append(i)
+
                 for i in tup_nextstep:
-                    stringOptions += "{})".format(i) + \
+                    stringOptions += "{})".format(tup_nextstep.index(i)+1) + \
                                      idAnswers_ByStep_Adventure[(i, game_context["id_step"])]["Description"] + "\n"
                 opc = funciones.getOpt(textOpts=stringOptions, inputOptText="Select Option ->",
-                                       rangeList=list(tup_nextstep))
+                                       rangeList=list_options)
 
-                print(idAnswers_ByStep_Adventure[(opc, game_context["id_step"])]["Resolution_Answer"])
+                print(idAnswers_ByStep_Adventure[(tup_nextstep[opc-1], game_context["id_step"])]["Resolution_Answer"])
                 print()
                 input("Press Enter to continue")
-                game_context["id_step"] = opc
+                game_context["id_step"] = tup_nextstep[opc-1]
                 funciones.insertCurrentChoice(game_context["id_game"], game_context["id_step"],
-                                              tup_options[tup_nextstep.index(opc)])
+                                    tup_options[tup_nextstep.index(tup_nextstep[opc-1])],game_context["id_adventure"])
                 idAnswers_ByStep_Adventure = funciones.get_answers_bystep_adventure()
             else:
                 funciones.getHeader(adventures[adv]["Name"])
                 print(id_by_steps[game_context["id_step"]]["Description"])
                 print("FINAL")
                 input("Press Enter to continue")
-                funciones.insertCurrentChoice(game_context["id_game"], game_context["id_step"], opc)
+                funciones.insertCurrentChoice(game_context["id_game"], game_context["id_step"], tup_nextstep[opc-1],
+                                              game_context["id_adventure"])
                 break
         flg_juego = False
         flg_10 = True
@@ -253,14 +265,92 @@ while True:
 
     # Flag para Replays
     while flg_3:
-        print("Falta por implementar")
-        input()
-        flg_3 = False
+        pos_menu = 0
+        tuple_idgames = funciones.getIdGames()
+        while True:
+            list_idgames = []
+            for i in tuple_idgames[pos_menu:pos_menu + 4]:
+                list_idgames[i] = tuple_idgames[i]
+
+            stringAdventures = funciones.getFormatedAdventures(dic_show)
+            adv = funciones.getOpt(stringAdventures, "What adventure do you want to play?(0 Go back)",
+                                   adventures_keys, exceptions=[0, "+", "-"])
+            if adv == "+":
+                if pos_menu + 4 > len(adventures_keys) - 4:
+                    if pos_menu + 4 == len(adventures_keys):
+                        pos_menu = 0
+                        print()
+                    else:
+                        pos_menu = len(adventures_keys) - 4
+                        print()
+                else:
+                    pos_menu += 4
+                    print()
+            elif adv == "-":
+                if pos_menu - 4 < 0:
+                    if pos_menu == 0:
+                        pos_menu = len(adventures_keys) - 4
+                        print()
+                    else:
+                        pos_menu = 0
+                        print()
+                else:
+                    pos_menu -= 4
+                    print()
+            else:
+                # Elije Opcion del menú
+                if adv == 0:
+                    flg_10 = True
+                    flg_chooseAdventure = False
+                    break
+                else:
+                    flg_juego = True
+                    flg_chooseAdventure = False
+                    break
+#         query = "select g.id_game as \"ID\", u.user_name as \"Username\",  a.adventure_name as \"Name\", c.character_name as \"Character Name\", g.date as \"Date\" from GAME g \
+# inner join CHOOSE_YOUR_ADVENTURE.USER u on g.id_user = u.id_user \
+# inner join ADVENTURE a on g.id_adventure = a.id_adventure \
+# inner join CHOOSE_YOUR_ADVENTURE.CHARACTER c on g.id_character = c.id_character \
+# order by g.id_game asc"
+#         funciones.getReplayHeader()
+#         print(funciones.getFormatedTable(funciones.get_table(query)))
+#         opt_replay = funciones.getOpt(inputOptText="What adventure do you want to replay?",rangeList=,exceptions=[0])
+#
+#         flg_3 = False
 
     # Flag para el menú de reports
     while flg_4:
-        print("\n\n")
+        print("\n"*17)
         funciones.getReportHeader()
         opc = funciones.getOpt(head_report_menu, menu_input, [1, 2, 3, 4])
-        if opc == 4:
+
+        if opc == 1:
+            funciones.getFormatedTable(funciones.get_table(f"")
+                                       ,"Most used answer")
+            input("Press Enter to continue")
+
+        elif opc == 2:
+            print(funciones.getFormatedTable(funciones.get_table(f"SELECT u.user_name, COUNT(g.id_user) AS veces_jugadas \
+    FROM GAME g \
+    JOIN CHOOSE_YOUR_ADVENTURE.USER u \
+    ON u.id_user = g.id_user \
+GROUP BY u.user_name \
+ORDER BY veces_jugadas DESC, u.date_creation asc \
+LIMIT 1"), "Player with more games Played"))
+            input("Press Enter to continue")
+
+        elif opc == 3:
+            print("What user do you want to see?:")
+            user_see = input()
+            if funciones.userExists(user_see):
+                print(funciones.getFormatedTable(funciones.get_table(f"SELECT a.id_adventure, a.adventure_name, g.date \
+    FROM ADVENTURE a JOIN GAME g ON  g.id_adventure = a.id_adventure \
+    JOIN CHOOSE_YOUR_ADVENTURE.USER u ON u.id_user = g.id_user      \
+    where u.id_user = (SELECT id_user FROM CHOOSE_YOUR_ADVENTURE.USER WHERE user_name = '{user_see}') \
+    ORDER BY g.date desc" ), "Games played by user"))
+                input("Press Enter to continue")
+            else:
+                print("The user:{} doesn't exist".format(user_see))
+                input("Press Enter to continue")
+        elif opc == 4:
             flg_4 = False
